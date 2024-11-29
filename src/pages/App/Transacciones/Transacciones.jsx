149c6c231ebc30fda55541f8a9862a160ddac323
santiclfriedrich <santiclear1@hotmail.com>
2024-11-29 17:43:32 -0300

    Transactions finalizado

--- src/pages/App/Transacciones/Transacciones.jsx
+++ src/pages/App/Transacciones/Transacciones.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Transacciones.css'; // Asegúrate de importar el archivo CSS
+import EditSvg from '../../../../public/assets/App/Transactions/edit.png'
+import DeleteSvg from '../../../../public/assets/App/Transactions/delete.png'
 
 const Transacciones = () => {
+
   // Datos hardcodeados de transacciones
   const transactions = [
     { id: 1, description: 'Pago de servicios', date: '2024-11-01', amount: -50.00, type: 'Egreso' },
@@ -11,9 +14,28 @@ const Transacciones = () => {
     { id: 5, description: 'Pago de alquiler', date: '2024-11-15', amount: -450.00, type: 'Egreso' }
   ];
 
+  //Logica de filtro por ingreso/egreso
+  const [filter, setFilter] = useState('Todos')
+
+  const filteredTransactions = transactions.filter((transaction) => {
+    if(filter === 'Todos') return true;
+    return transaction.type === filter
+  })
+
+  
+
   return (
     <div className="transacciones-container">
       <h1 className="transacciones-title">Transacciones</h1>
+
+      <div className='container-mini-menu'>
+        <ul className='mini-menu'>
+          <li className={`menu-option ${filter === 'Todos' ? 'active' : ''}`} onClick={() => setFilter('Todos') }>Todos</li>
+          <li className={`menu-option ${filter === 'Ingreso' ? 'active' : '' } `} onClick={() => setFilter('Ingreso') } >Ingresos</li>
+          <li className={`menu-option ${filter === 'Egreso' ? 'active' : '' } `} onClick={() => setFilter('Egreso')} >Egresos</li>
+        </ul>
+      </div>
+
       <table className="transacciones-table">
         <thead>
           <tr>
@@ -21,10 +43,12 @@ const Transacciones = () => {
             <th>Fecha</th>
             <th>Monto</th>
             <th>Tipo</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
           </tr>
         </thead>
         <tbody>
-          {transactions.map((transaction) => (
+          {filteredTransactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.description}</td>
               <td>{transaction.date}</td>
@@ -33,6 +57,12 @@ const Transacciones = () => {
               </td>
               <td className={transaction.type === 'Ingreso' ? 'income' : 'expense'}>
                 {transaction.type}
+              </td>
+              <td>
+                <img src={EditSvg} alt="edit" className='edit-icon' />
+              </td>
+              <td>
+                <img src={DeleteSvg} alt="delete" className='delete-icon' />
               </td>
             </tr>
           ))}
